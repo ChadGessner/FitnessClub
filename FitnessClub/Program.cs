1bc388060e128934ac8721026efef4e1ccf6f279
ChadGessner <108926447+ChadGessner@users.noreply.github.com>
2022-11-18 12:07:15 -0500

    Added Get methods for pulling data from DataService() Made Lists in DataService() available only through these methods for encapsulation Methods are listed at the top of program.cs 11/18/22

--- FitnessClub/Program.cs
+++ FitnessClub/Program.cs
@@ -10,7 +10,7 @@ Optional enhancements:
 (Easy/Medium) Allow new members to receive discounts if they sign up during certain time periods, explore the DateTime library for help with date and time.
 (Medium) Store clubs and members in text files.
 (Hard) Out Pizza the hut 
- */
+ 
 using FitnessClub;
 Console.WriteLine("Welcome to Pizza Hut Gym!");
 DataService data = new DataService();
@@ -93,3 +93,79 @@ void CreateMember()
 
 
 
+=======
+string clubOne1 = "Club One|123 Oak Street|400";
+string clubTwo2 = "Club Two|133 Oak Street|500";
+*/
+
+
+
+Console.WriteLine("Welcome to Pizza Hut Gym!");
+//check if user is already registered here, if not call CreateMember method below
+CreateMember();
+
+void CreateMember()
+{
+    Console.Write("Please enter your name:");
+    string userName = Console.ReadLine();
+    bool isValidDate = false;
+    DateTime dateOfBirth = default(DateTime);
+    while (!isValidDate)
+    {
+        Console.WriteLine("Please enter your date of birth (mm/dd/yyyy):");
+        string dateInput = Console.ReadLine();
+        if (!Validation.IsDate(dateInput))
+        {
+            isValidDate = false;
+            Console.WriteLine("Please enter a valid date format.");
+        }
+        else
+        {
+            isValidDate = true;
+            dateOfBirth = DateTime.Parse(dateInput);
+            break;
+        }
+    }
+    bool memberTypeValid = false;
+    string memberType = "";
+    while (!memberTypeValid)
+    {
+        Console.Write($"Which membership option would you prefer? Enter 'single' for access to one club or 'multi' for access to all clubs.");
+        memberType = Console.ReadLine().ToLower();
+        switch (memberType)
+        {
+            case "single":
+                memberTypeValid = true;
+                //Choose single club method here
+                //create single user method here
+                break;
+            case "multi":
+                memberTypeValid = true;
+                CreateMultiMember(userName, dateOfBirth);
+                break;
+            default:
+                memberTypeValid = false;
+                Console.WriteLine("That is not a valid input, please try again.");
+                break;
+        }
+    }
+
+
+    void CreateMultiMember(string userName, DateTime dateOfBirth)
+    {
+        MultiMember member = new MultiMember()
+        {            
+            //Id = How do we determine the next ID to use? Do we need to read the whole file into a list first?
+            FullName = userName,
+            DateOfBirth = dateOfBirth,
+            JoinDate = DateTime.Now,
+
+        };
+        data.AddMember(member);
+    }
+
+
+    Console.WriteLine("complete");
+
+}
+*/
